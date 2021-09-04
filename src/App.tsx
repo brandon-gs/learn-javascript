6167fd4a81770069828155010f6f654c8c0074c0
BrandonGS <brandongs180@gmail.com>
2021-09-03 21:38:53 -0500

    Add key to Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import pages from "./utils/pages";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        {pages.map((page) => (
-          <Route path={page.path}>{page.component}</Route>
-        ))}
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                {pages.map((page) => (
+                    <Route key={`page-${page.path}`} path={page.path}>
+                        {page.component}
+                    </Route>
+                ))}
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
